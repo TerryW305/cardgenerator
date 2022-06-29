8e8367a68154a52abec332ca537021285e458343
TerryW305 <terrywelch30625@gmail.com>
2022-06-29 00:43:39 +0000

    cardgenerator

--- src/app.js
+++ src/app.js
@@ -4,8 +4,40 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { right } from "@popperjs/core";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  setInterval(randomCard, 1000);
 };
+function randomCard() {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "K",
+    "Q"
+  ];
+  let suits = ["♦", "♥", "♠", "♣"];
+  let randNum = Math.floor(Math.random() * numbers.length);
+  document.querySelector(".cardnumber").innerText = numbers[randNum];
+  let randSuits = Math.floor(Math.random() * suits.length);
+  document.querySelector(".topsuit").innerText = suits[randSuits];
+  document.querySelector(".bottomsuit").innerText = suits[randSuits];
+  if (suits[randSuits] == "♥" || suits[randSuits] == "♦") {
+    document.querySelector(".topsuit").classList.add("Red");
+    document.querySelector(".bottomsuit").classList.add("Red");
+    document.querySelector(".cardnumber").classList.add("Red");
+  } else {
+    document.querySelector(".topsuit").classList.remove("Red");
+    document.querySelector(".bottomsuit").classList.remove("Red");
+    document.querySelector(".cardnumber").classList.remove("Red");
+  }
+}
